986a12ddb9e306a3bc42aea29f119bc42b246232
kish4707 <121122191+kish4707@users.noreply.github.com>
2023-08-10 17:53:22 +0900

    Add files via upload

---
+++ HistApplication.java
@@ -0,0 +1,13 @@
+package com.hist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistApplication.class, args);
+	}
+
+}
