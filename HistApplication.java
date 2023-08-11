7d449c65fef7f9a9fb3a6349c386d265fdd455cf
kish4707 <121122191+kish4707@users.noreply.github.com>
2023-08-11 16:58:43 +0900

    Update HistApplication.java

--- HistApplication.java
+++ HistApplication.java
@@ -1,13 +1,1 @@
-package com.hist;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HistApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HistApplication.class, args);
-	}
-
-}
